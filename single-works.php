40bb12fb3a3640346e3d9e7c0ef47d1ca1391212
akiko262 <58Akinko@gmail.com>
2024-08-27 14:52:53 +0900

    更新

--- single-works.php
+++ single-works.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-
 <main class="project-detail">
     <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
@@ -23,90 +22,86 @@
                     </div>
                     <?php the_content(); ?>
                     <section class="works-contents">
-                        <h4>Skills</h4>
+                        <h4><span class="eng">Skills</span></h4>
                         <div class="skill-wrapper">
-                        <ul class="skill-list">
+                            <ul class="skill-list">
+                            <?php
+                            // ACFのカスタムフィールドから選択されたスキルを取得
+                            $skills = get_field('skills'); // フィールド名が 'skills' の場合
+
+                            // 使用言語に対応するFontAwesomeのクラス名を設定
+                            $icons = array(
+                                'HTML' => 'fa-brands fa-html5 fa-4x custom-icon',
+                                'CSS' => 'fa-brands fa-css3-alt fa-4x custom-icon',
+                                'JavaScript' => 'fa-brands fa-js fa-4x custom-icon',
+                                'PHP' => 'fa-brands fa-php fa-4x custom-icon',
+                                'WordPress' => 'fa-brands fa-wordpress fa-4x custom-icon',
+                            );
+
+                            // チェックボックスで選択されたスキルをループ
+                            if( $skills ):
+                                foreach( $skills as $skill ):
+                                    // クラス名が設定されていれば表示
+                                    if( array_key_exists( $skill, $icons ) ): ?>
+                                        <li><i class="<?php echo esc_attr( $icons[$skill] ); ?>"></i></li>
+                                    <?php
+                                    endif;
+                                endforeach;
+                            endif;
+                            ?>
+                            </ul>
+                        </div>
                         <?php
-        // ACFのカスタムフィールドから選択された言語を取得
-        $skills = get_field('skills'); // フィールド名が 'skills' の場合
-        
-        // 使用言語に対応するFontAwesomeのクラス名を設定
-        $icons = array(
-            'HTML' => 'fa-brands fa-html5 fa-4x custom-icon',
-            'CSS' => 'fa-brands fa-css3-alt fa-4x custom-icon',
-            'JavaScript' => 'fa-brands fa-js fa-4x custom-icon',
-            'PHP' => 'fa-brands fa-php fa-4x custom-icon',
-            'wordpress' => 'fa-brands fa-wordpress fa-4x custom-icon',
-        );
+// 詳細を表示
+$detail = get_field('detail');  // "detail" はフィールド名
+if ($detail) {
+    echo '<details class="accordion-003">';
+    echo '<summary>詳細</summary>';
+    echo '<ul>';
+    $detail_list = explode("\n", $detail);
+    foreach ($detail_list as $item) {
+        if (!empty(trim($item))) {
+            echo '<li>' . esc_html(trim($item)) . '</li>';
+        }
+    }
+    echo '</ul>';
+    echo '</details>';
+} else {
+    echo '<p>詳細はありません。</p>';
+}
 
-        // チェックボックスで選択された言語をループ
-        if( $skills ):
-            foreach( $skills as $skill ):
-                // クラス名が設定されていれば表示
-                if( array_key_exists( $skill, $icons ) ): ?>
-                    <li><i class="<?php echo esc_attr( $icons[$skill] ); ?>"></i></li>
-                <?php
-                endif;
-            endforeach;
-        endif;
-        ?>
-        </ul>
-                            <?php
-                            $github_link = get_field('github_link');
-                            if ($github_link) {
-                                echo '<a href="' . esc_url($github_link) . '" target="_blank">GitHubでコードを見る</a>';
-                            } else {
-                                echo '<p>GitHubリンクはありません</p>';
-                            }
-                            ?>
-                        </div>
-                        <div class="project-info">
-                            <h4>Point</h4>
-                            <?php
-                            $design_tips = get_field('design_tips');
-                            if ($design_tips) {
-                                echo '<details class="accordion-003">';
-                                echo '<summary>工夫やこだわったところ</summary>';
-                                echo '<ul>';
-                                $tips = explode("\n", $design_tips);
-                                foreach ($tips as $tip) {
-                                    if (!empty(trim($tip))) {
-                                        echo '<li>' . esc_html(trim($tip)) . '</li>';
-                                    }
-                                }
-                                echo '</ul>';
-                                echo '</details>';
-                            } else {
-                                echo '<p>工夫やこだわったところはありません。</p>';
-                            }
+// 学びを表示
+$study = get_field('study');  // "study" はフィールド名
+if ($study) {
+    echo '<details class="accordion-003">';
+    echo '<summary>学び</summary>';
+    echo '<ul>';
+    $study_list = explode("\n", $study);
+    foreach ($study_list as $item) {
+        if (!empty(trim($item))) {
+            echo '<li>' . esc_html(trim($item)) . '</li>';
+        }
+    }
+    echo '</ul>';
+    echo '</details>';
+} else {
+    echo '<p>学びはありません。</p>';
+}
+?>
 
-                            $challenges = get_field('challenges');
-                            if ($challenges) {
-                                echo '<details class="accordion-003">';
-                                echo '<summary>苦労したところ</summary>';
-                                echo '<ul>';
-                                $challenges_list = explode("\n", $challenges);
-                                foreach ($challenges_list as $challenge) {
-                                    if (!empty(trim($challenge))) {
-                                        echo '<li>' . esc_html(trim($challenge)) . '</li>';
-                                    }
-                                }
-                                echo '</ul>';
-                                echo '</details>';
-                            } else {
-                                echo '<p>苦労したところはありません。</p>';
-                            }
 
-                            $production_period = get_field('Production_period');
-                            if ($production_period) {
-                                echo '<h4>製作期間</h4>';
-                                echo '<p>' . esc_html($production_period) . '</p>';
-                            } else {
-                                echo '<h4>製作期間</h4>';
-                                echo '<p>情報がありません。</p>';
-                            }
-                            ?>
-                        </div>
+                    
+                        <?php
+                        // 製作期間を表示
+                        $production_period = get_field('Production_period');
+                        if ($production_period) {
+                            echo '<h4 class="eng-2">製作期間</h4>';
+                            echo '<p>' . esc_html($production_period) . '</p>';
+                        } else {
+                            echo '<h4 class="eng-2">製作期間</h4>';
+                            echo '<p>情報がありません。</p>';
+                        }
+                        ?>
                     </section>
                 </div>
             </article>
