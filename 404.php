205a98402eb19e603dbe9cdf76762693184f0e9a
akiko262 <58Akinko@gmail.com>
2024-08-28 20:35:54 +0900

    更新

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<div class="error-404">
+        <h2 class="error-title">404</h2>
+        <p class="error-message">ページが見つかりませんでした<br>
+        お探しのページは、移動もしくは削除された可能性があります。</p>
+        <a href="<?php echo home_url(); ?>" class="error-link">ホームに戻る</a>
+</div>
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -20,7 +20,7 @@
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="/works">Works</a></li>
                 <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="<?php echo home_url(); ?>/#contact">Contact</a></li>
             </ul>
         </nav>
         </div>
